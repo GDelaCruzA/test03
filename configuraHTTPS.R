e30e0ff7266d4928180f8cf4df546a23588cb16e
GDelaCruzA <gaguero@ipn.mx>
2019-05-15 20:05:16 -0600

    cierre de instrucciones para registro HTTPS

--- configuraHTTPS.R
+++ configuraHTTPS.R
@@ -21,6 +21,7 @@
 # saber su ruta de acceso, p.e. d:/githubrepos/prueba01
 # saber el url del repo en github, p.e. https://github.com/gdelac/prueba01
 # verificar que el repo remoto esté configurado como tal:
+# 
 #       git remote -v
 # 
 # la respuesta son dos líneas que confirman que fetch y push apuntan al repo
@@ -35,4 +36,21 @@
 # 
 # debe arrojar una respuesta parecida a la siguiente:
 # 
+#  master 92c6971 [origin/master] configuracion del acceso via HTTPS
+# 
+# Para sembrar las credenciales debemos realizar el proceso de actualización
+# del repo remoto en github, de la sigugitiente manera:
+# 
+# En RStudio, crear o editar un archivo existente (puede ser el README.md o
+# un script de R existente o nuevo), guardarlo como es usual y luego, como es
+# usual, en la pestaña Git, señalar o habilitar la columna Staged, pulsar el
+# botón Commit, acompletar la descripción de la actualización y pulsar el 
+# botón Commit de nuevo, cerrar la ventana y después pulsar el botón con la
+# flecha verde hacia arriba (Push) y verificar que no haya error.
+# 
+# Este procedimiento podría generar un diálogo para proporcionar la contraseña
+# de github; debería ser la última vez que la requiera. En lo sucesivo, toda
+# la actividad relacionada con el repo remoto (Github) será automática, sin
+# el requerimiento de las credenciales de acceso desde el equipo en el que
+# hicimos la activación del manejo de credenciales.
 # 
