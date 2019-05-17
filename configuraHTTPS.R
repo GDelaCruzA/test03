99e3c7eed0923954451e1a692d262a3006e25d37
gdelac-lapaz <gaguero@ipn.mx>
2019-05-16 20:34:22 -0600

    pasos del (2) al (5) en MVac11 (Linux_AMD-a8)

--- configuraHTTPS.R
+++ configuraHTTPS.R
@@ -6,7 +6,7 @@
 # 
 # Verificar cual es el credential helper del SO abriendo una sesión en la Terminal
 # 
-#     git config --show-origin --get credential.helper
+# (1)    git config --show-origin --get credential.helper
 # 
 # dependiendo del resultado:
 # 
@@ -15,14 +15,14 @@
 # de configuración usará la última palabra (manager o wincred) en la definición
 # del ayudante de credenciales del caso (usualmente en Windows es wincred)
 # 
-#     git config --global credential.helper wincred
+# (2)    git config --global credential.helper wincred
 # 
 # Debemos de tener un repo de prueba
 # saber su ruta de acceso, p.e. d:/githubrepos/prueba01
 # saber el url del repo en github, p.e. https://github.com/gdelac/prueba01
 # verificar que el repo remoto esté configurado como tal:
 # 
-#       git remote -v
+# (3)      git remote -v
 # 
 # la respuesta son dos líneas que confirman que fetch y push apuntan al repo
 # en github:
@@ -32,7 +32,7 @@
 # confirmamos que el repo local es la rama 'master' bajo control de github;
 # si no hay conflictos, el comando:
 # 
-#     git branch -vv
+# (4)    git branch -vv
 # 
 # debe arrojar una respuesta parecida a la siguiente:
 # 
@@ -41,9 +41,9 @@
 # Para sembrar las credenciales debemos realizar el proceso de actualización
 # del repo remoto en github, de la siguiente manera:
 # 
-# En RStudio, crear o editar un archivo existente (puede ser el README.md o
-# un script de R existente o nuevo), guardarlo como es usual y luego, como es
-# usual, en la pestaña Git, señalar o habilitar la columna Staged, pulsar el
+# (5) En RStudio, crear o editar un archivo existente (puede ser el README.md o
+# un script de R existente o nuevo), guardarlo y luego, como es usual,
+# en la pestaña Git, señalar o habilitar la columna Staged, pulsar el
 # botón Commit, acompletar la descripción de la actualización y pulsar el 
 # botón Commit de nuevo, cerrar la ventana y después pulsar el botón con la
 # flecha verde hacia arriba (Push) y verificar que no haya error.
@@ -59,3 +59,19 @@
 #
 # editando de nuevo para verificar al credential helper de Win8.1 (manager)
 # Hecho...!!! ya no preguntó nada. OK.
+#
+# Para Linux
+# editando la configuración HTTPS en MVac11 (LinuxAMD-a8) desde RStudio
+# .... repetimos el paso (2) con la siguiente instrucción:
+#
+#           git config --global credential.helper 'cache --timeout=10000000'
+#
+# verificamos (3) y (4)
+#
+#           git remote -v
+# y
+#           git branch -vv
+#
+# el paso (5) guardando estas adiciones al texto...
+# 
+
